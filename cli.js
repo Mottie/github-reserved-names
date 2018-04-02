5cc852cfa01fc0af0ca7856a79ad4c57b1230d78
Rob Garrison <wowmotty@gmail.com>
2018-04-01 21:53:32 -0500

    Fix CLI

--- cli.js
+++ cli.js
@@ -3,8 +3,8 @@
 const meow = require("meow");
 const reservedNames = require("./");
 
-const cli = meow([
-	`Examples
+const cli = meow(`
+Examples
   $ github-reserved issues
   true
 
@@ -15,7 +15,7 @@ const cli = meow([
 
 Options
   --all   Show all reserved names`
-]);
+);
 
 console.log(
 	cli.flags.all ?
