792edd9699fb3643b4ca3f83fa0651f8c9fb3a0e
Federico <me@fregante.com>
2024-07-31 03:20:29 +0700

    Non-breaking

--- index.d.ts
+++ index.d.ts
@@ -2,20 +2,19 @@
 // Project: https://github.com/Mottie/github-reserved-names
 // Definitions by: Kid <https://github.com/kidonng>
 
-// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/consistent-type-definitions -- TODO: In the next major release
-interface oddball {
+type Oddball = {
 	reserved: boolean;
 	taken: boolean;
 	typical: boolean;
 	included: boolean;
 	redirect?: string;
-}
+};
 
 declare const reservedNames: {
 	all: string[];
 	check(name: string): boolean;
 	oddballs(): string[];
-	oddballs(name: string): oddball | string[];
+	oddballs(name: string): Oddball | string[];
 };
 
 export = reservedNames;
